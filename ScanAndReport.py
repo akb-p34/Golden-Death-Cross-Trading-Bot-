0d2c3156eef671d586fae86dc178d7e5c9cadf54
Akbar Pathan <akbar.pathan034@gmail.com>
2023-01-12 19:25:48 -0500

    retrieved ticker name data into one data frame

--- ScanAndReport.py
+++ ScanAndReport.py
@@ -6,10 +6,24 @@ title: GDC strategy
 
 '''
 What does it do:
-- Downloads all ticker symbols for constituencies in the S&P 500, S&P 400, and the SNP 600.
+- Downloads all ticker symbols for constituencies in the S&P 500, S&P 400, and the S&P 600.
 - it calculates the 50 day and 200 day simple moving average for each constituency
 - it calculates each time is the 50 day and 200 day simple moving average crosses each other in the past 30 days
 - raw close price data of the current day and the day before will be used to calculate the average rate of change of the 50 day moving average and 200 day moving average and whichever slope is greater determines if it is a death cross or Golden Cross
 - if a stock has a cross in the past 30 days then it will be recorded into a buy list or sell list depending if it's a golden or death cross
 '''
+
+import pandas as pd
+
+# get the ticker symbols of the stocks in the S&P 500
+df500 = pd.read_csv('sp-500-index-01-12-2023.csv')["Symbol"]
+
+# get the ticker symbols of the stocks in the S&P 400
+df400 = pd.read_csv('sp-400-index-01-12-2023.csv')["Symbol"]
+
+# get the ticker symbols of the stocks in the S&P 600
+df600 = pd.read_csv('sp-600-index-01-12-2023.csv')["Symbol"]
+
+# combine all three data frames together
+df1500 = pd.concat([df500, df400, df600], ignore_index = True)
 
